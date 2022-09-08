f7629cca852b4940541999c86b2e3b0ffdd97529
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-09-08 11:21:12 -0500

    9

---
+++ main9.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded",()=>{
+    let  valor=0,pi=3.14,r=0,h=0
+    valor=Number(prompt("Ingresa el primer valor"));
+    r=Number(prompt("Ingrese el radio del cilindro: "))
+    h=Number(prompt("Ingresa la altura: "))
+    let area=0,volumen=0
+    area=(valor*Math.PI*Math.pow(r,2))+((valor*pi*r)*h)
+    volumen=((pi*r**2)*h)
+
+    console.log(`El area del cilindro es de: ${area}`)
+    console.log(`El volumen del ciliindro es de: ${volumen}`)
+
+})
