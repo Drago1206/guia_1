1cd880b7d7b2ca5ebff94f31e7624d587b046be6
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-09-08 08:14:04 -0500

    4

---
+++ main4.js
@@ -0,0 +1,7 @@
+addEventListener("DOMContentLoaded",()=>{
+    let n1=0, n2=0,m=0
+    n1=Number(prompt("Ingrese el primer valor para calcular el area de un triangulo",4));
+    n2=Number(prompt("Ingresa el segundo valor para calcular el area del triangulo: ",3));
+    m=(n1*n2)/2
+    console.log(`El area del triangulo es: ${m}`)
+})
