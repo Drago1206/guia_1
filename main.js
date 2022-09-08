f7ebd0185eb8995aaebc069dcd4c86c1934fadd8
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-09-08 07:46:53 -0500

    2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 addEventListener("DOMContentLoaded",()=>{
     let n1=0 , n2=0 , n3=0
-    n1=Number(prompt("Ingrese el primer numero:  ",3))
-    n2=Number(prompt("Ingrese el segundo numero:  ",4))
-    n3=Number(prompt("Ingrese el tercer numero: ",5))
-    console.log(`La suma es de ${n1} + ${n2} + ${n3} = ${n1+n2+n3}`)
-    console.log(`La resta es de ${n1} - ${n2} - ${n3} =${n1-n2-n3}`)
-    console.log(`La multiplicacion es ${n1} * ${n2} * ${n3} = ${n1*n2*n3}`)
-    console.log(`La division es de ${n1} / ${n2} / ${n3} = ${n1/n2/n3}`)
+    n1=Number(prompt("Ingrese el primer numero:  ",3));
+    n2=Number(prompt("Ingrese el segundo numero:  ",4));
+    n3=Number(prompt("Ingrese el tercer numero: ",5));
+    console.log(`La suma es de ${n1} + ${n2} + ${n3} = ${n1+n2+n3}`);
+    console.log(`La resta es de ${n1} - ${n2} - ${n3} =${n1-n2-n3}`);
+    console.log(`La multiplicacion es ${n1} * ${n2} * ${n3} = ${n1*n2*n3}`);
+    console.log(`La division es de ${n1} / ${n2} / ${n3} = ${n1/n2/n3}`);
 })
---
+++ main2.js
@@ -0,0 +1,7 @@
+addEventListener("DOMContentLoaded",()=>{
+    let ld1=0, ld2=0,  m=0
+    ld1=Number(prompt("Ingrese el valor del primer lado: ",3));
+    ld2=Number(prompt("Ingresa el valor del segundo lado: ",5));
+    m=(ld1*ld2)
+    console.log(`El area del rectangulo es de  ${ld1} * ${ld2}=${ld1*ld2}`)
+})
